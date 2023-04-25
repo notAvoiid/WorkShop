c95c5821c0cad30fcc9385e2a682c9a039c25060
Igor Nascimento <igor.nacimento51@gmail.com>
2023-04-25 15:38:01 -0300

    Initial commit

---
+++ src/main/java/com/mrzoom/startproject/StartprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mrzoom.startproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartprojectApplication.class, args);
+    }
+
+}
