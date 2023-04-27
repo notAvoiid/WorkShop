f3f8c98ccceb94095270b6029f60d5b0557df781
Igor Nascimento <igor.nacimento51@gmail.com>
2023-04-27 14:54:33 -0300

    "User Delete"

--- src/main/java/com/mrzoom/startproject/controllers/UserController.java
+++ src/main/java/com/mrzoom/startproject/controllers/UserController.java
@@ -36,4 +36,9 @@ public class UserController {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
